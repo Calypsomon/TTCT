4ba983b1c42a4dc7e29de1a92ef4e762198c27ed
Calypsomon <Calypsomonn@gmail.com>
2024-11-09 16:02:10 +0100

    modified:   Tree.java

--- Tree.java
+++ Tree.java
@@ -52,9 +52,85 @@ public class Tree {
         teamlist[x].removePoint(point);
     }
     
-    public void Round1(){
-    
-    
+    public void FFA(){
+    /*if (teamcount%2 != 0) {
+        Team hTeam = new Team("FreeWin","WIN",teamlist[0].getTeamsize());
+    }*/
+    int matchcode = 0;
+    int zähler = teamcount - teamcount/2;
+    int a,i;
+    int x = 1;
+    int y = 1;
+    match[] match = new match[1];
+        for (i= 1;i<teamcount-1;i++){
+            System.out.println("Runde" + i + "\n");
+    for (a= 0;a<zähler;a++){
+    matchcode = matchcode + 1;
+    match[matchcode] = new match(teamlist[x],teamlist[y],matchcode);
     }
-    
+    }}
+    public void Swiss(){
+           
+            Team hteam = new Team("FreeWin187","WIN",0);
+            int matchcode = 0;
+            int zähler = teamcount - teamcount/2;
+            int a,i, y= 1, x= 2;
+            match[] Matches = new match[zähler];
+            
+                    System.out.println("Runde 1 \n");
+            for (a= 0;a<zähler;a++){
+            matchcode ++;
+            if (x<=teamcount){
+        	    Matches[matchcode-1] = new match(teamlist[y],teamlist[x],matchcode);
+            } else {
+                Matches[matchcode-1] = new match(teamlist[y],hteam,matchcode);
+            }
+            y = y + 2;
+            x = x + 2;
+        }
+            for (i=1;i<berechneRunden(teamcount);i++){
+                    boolean allFinished = false;
+                    while (!allFinished) {
+                        allFinished = true; 
+                        for (int f = 0; f < Matches.length; f++) {
+                            if (!Matches[f].finished()) {  // Falls ein Match noch nicht fertig ist
+                                allFinished = false;  // Setze allFinished auf false und verlasse die Schleife
+                                break;
+                            }
+                        }
+                
+                        if (!allFinished) {
+                            try {
+                                Thread.sleep(5000);  // Warte 5 Sekunden
+                            } catch (InterruptedException e) {
+                                Thread.currentThread().interrupt();  // Falls der Thread unterbrochen wird, die Schleife verlassen
+                                break;
+                            }
+                        }
+                    }
+                
+                zähler = zähler/2;
+                y=1;x=2;
+                match[] Matches2= new match[zähler];
+                for (a= 0;a<zähler;a++){ 
+                    matchcode ++;
+                    Matches2[a] = new match(Matches[y].getWinner(),Matches[x].getWinner(),matchcode);
+                    y = y + 2;
+                    x = x + 2;
+                }
+                Matches = Matches2;
+            }
+    }
+
+    int berechneRunden(int teilnehmer) {
+    int runden = 0;
+    if (teamcount%2 != 0) {
+        teilnehmer++;
+    }
+    while (teilnehmer > 1) {
+        teilnehmer /= 2;
+        runden++;
+    }
+    return runden ;
+    }
 } //by Calypsomon
